354965440af82ab799d6b872b3e5c1054dffb136
p-nuts5963 <polythene.pam5963@gimail.com>
2022-01-13 02:39:17 +0900

    add app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,11 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
 import PostList from "../components/PostList";
 
-const posts = [
-  {
-    slug: "react",
-    title: "Reactサイコー！",
-    content: `<p>ReactはFacebookの提供するUIライブラリ。コンポーネント志向ですごく書きやすい。</p>`,
-    publishedAt: "2020.12.21 14:23",
-    category: {
-      slug: "dev",
-      name: "開発",
-    },
-    thumbnail: {
-      url: "/gatsby-icon.png",
-    },
-  },
-  {
-    slug: "figma",
-    title: "Figmaはいいぞ",
-    content: "<p>Figmaの良さが伝わると嬉しい。</p>",
-    publishedAt: "2020.12.22 23:11",
-    category: {
-      slug: "design",
-      name: "デザイン",
-    },
-    thumbnail: {
-      url: "/gatsby-icon.png",
-    },
-  },
-  {
-    slug: "gatsby",
-    title: "Gatsbyもいいぞ",
-    content: "<p>Gatsbyを使うと爆速サイトができるからいいよ</p>",
-    publishedAt: "2020.12.23 07:34",
-    category: {
-      slug: "dev",
-      name: "開発",
-    },
-    thumbnail: {
-      url: "/gatsby-icon.png",
-    },
-  },
-];
-
-const IndexPage = () => {
+const IndexPage = (props) => {
+  const posts = props.data.allMicrocmsPost.nodes;
   return (
     <Layout>
       <Helmet>
@@ -59,5 +19,25 @@ const IndexPage = () => {
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    allMicrocmsPost {
+      nodes {
+        slug
+        title
+        content
+        publishedAt(formatString: "YYYY.DD.MM hh:mm")
+        category {
+          slug
+          name
+        }
+        thumbnail {
+          url
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage;
